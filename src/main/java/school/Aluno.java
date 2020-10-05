47095470001fe54b5735fe3d97650befef600bc8
rodrigobdias <rodrigobdias@gmail.com>
2020-10-05 16:31:21 -0300

    Classe de indicações

--- src/main/java/school/Aluno.java
+++ src/main/java/school/Aluno.java
@@ -3,17 +3,33 @@ package school;
 import java.util.ArrayList;
 import java.util.List;
 
-public class student {
+public class Aluno {
 
 	private CPF cpf;
 	private String name;
 	private Email email;
-	
 	private List<Telefone> telefones = new ArrayList<>();
 	
+	public Aluno(CPF cpf, String name, Email email) {
+		this.cpf = cpf;
+		this.name = name;
+		this.email = email;
+	}
+
 	public void adicionarTelefone(String ddd, String numero) {
 		this.telefones.add(new Telefone(ddd, numero));
 	}
 	
+	public CPF getCpf() {
+		return cpf;
+	}
+	
+	public Email getEmail() {
+		return email;
+	}
+	
+	public String getName() {
+		return name;
+	}
 	
 }
