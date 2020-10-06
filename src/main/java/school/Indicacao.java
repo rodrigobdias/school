c922bbfa0f55c8bed37208f1ff589c57b01ddc50
rodrigobdias <rodrigobdias@gmail.com>
2020-10-06 10:21:58 -0300

    Factories

--- src/main/java/school/Indicacao.java
+++ src/main/java/school/Indicacao.java
@@ -1,16 +1,29 @@
 package school;
 
+import java.time.LocalDateTime;
+
 public class Indicacao {
 
 	private Aluno indicado;
 	private Aluno indicante;
+	private LocalDateTime dataIndicacao;
 	
 	public Indicacao(Aluno indicado, Aluno indicante) {
 		this.indicado = indicado;
 		this.indicante = indicante;
+		this.dataIndicacao = LocalDateTime.now();
+	}
+
+	public LocalDateTime getDataIndicacao() {
+		return dataIndicacao;
 	}
 	
+	public Aluno getIndicado() {
+		return indicado;
+	}
 	
-	
+	public Aluno getIndicante() {
+		return indicante;
+	}	
 	
 }
